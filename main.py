cbfce6f859d3e698f6a1dd3bac56c8ba68a3ad51
gracejoh <ganwynj@icloud.com>
2020-07-22 15:18:07 +0000

    .

---
+++ main.py
@@ -0,0 +1,18 @@
+num = input("choose a number 1-4: ")
+
+if num == "1" :
+  print("The Graellsia Isabellae moth is also known as the stained glass moth or the spanish moon moth.")
+
+elif num == "2" :
+  print("The Graellsia Isabellae moth is endemic to france and spain.")
+
+elif num == "3" :
+  print("The Graellsia Isabellae moth likes to live in high mountains such as the Alps and Pyrenees.")
+
+elif num == "4" :
+  print("The Graellsia Isabellae moth has a protected status in Europe, which means that it is forbidden to harm or destroy them. ")
+
+else:
+  print("please enter a valid number")
+
+
